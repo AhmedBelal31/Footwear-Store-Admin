613dd3eefdc11c137a42e420010d57385b954fc7
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-05-31 12:07:47 +0300

    Apply Font For All App

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:footwear_store_admin/presentation/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'core/utils/bloc_observer.dart';
 import 'firebase_options.dart';
@@ -27,11 +28,14 @@ class FootWearStore extends StatelessWidget {
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-
+        textTheme: GoogleFonts.almaraiTextTheme(
+          Theme.of(context).textTheme,
+        ),
         scaffoldBackgroundColor: Colors.white ,
         appBarTheme:const  AppBarTheme(
           backgroundColor: Colors.white
-        )
+        ),
+
       ),
       debugShowCheckedModeBanner: false,
       // ? Navigate From Home To Add New Products .
