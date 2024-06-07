fbdeeb92f1fbae8071230e366c3b01b7a2d5200b
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-07 10:01:24 +0300

    Change Home UI

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -3,249 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:footwear_store_admin/data/models/product_model.dart';
 import 'package:footwear_store_admin/presentation/controller/product_cubit.dart';
 import 'package:footwear_store_admin/presentation/screens/add_product_screen.dart';
+import 'package:footwear_store_admin/presentation/widgets/custom_snack_bar.dart';
 import 'package:footwear_store_admin/styles.dart';
 import 'orders_screen.dart';
 import 'dart:math';
-
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
-
-//
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocConsumer<ProductCubit, ProductStates>(
-//       listener: (context, state) {
-//         if (state is DeleteProductSuccessState) {
-//           CustomSnackBarOverlay.show(
-//             context,
-//             message: 'Deleted',
-//             messageDescription: 'Product Deleted !',
-//             msgColor: Colors.red,
-//           );
-//         }
-//       },
-//       builder: (context, state) {
-//         var cubit = BlocProvider.of<ProductCubit>(context);
-//         return Scaffold(
-//           appBar: AppBar(
-//             scrolledUnderElevation: 0.0,
-//             title: const Text("Footwear Products"),
-//             centerTitle: true,
-//             // actions: [
-//             //   if (state is GetProductLoadingState)
-//             //     const CircularProgressIndicator(color: AppStyles.kPrimaryColor),
-//             //   if (state is! GetProductLoadingState)
-//             //     IconButton(
-//             //       onPressed: () {
-//             //         Navigator.of(context).push(
-//             //           MaterialPageRoute(
-//             //             builder: (context) => OrdersScreen(
-//             //               orders: BlocProvider.of<ProductCubit>(context).orders,
-//             //             ),
-//             //           ),
-//             //         );
-//             //       },
-//             //       icon: const Icon(Icons.menu),
-//             //     ),
-//             //   const SizedBox(width: 10),
-//             // ],
-//           ),
-//           floatingActionButton: FloatingActionButton(
-//             onPressed: () {
-//               Navigator.of(context).push(
-//                 MaterialPageRoute(
-//                   builder: (context) => const AddProductScreen(),
-//                 ),
-//               );
-//             },
-//             child: const Icon(Icons.add),
-//           ),
-//           body: Builder(
-//             builder: (context) {
-//               if (cubit.products.isEmpty && state is! GetProductLoadingState) {
-//                 return const Center(child: Text('There Is No Products '));
-//               } else {
-//                 if (state is GetProductFailureState) {
-//                   return Text('Error: ${state.error}');
-//                 } else if (state is DeleteProductFailureState) {
-//                   return Text('Error: ${state.error}');
-//                 } else {
-//                   return Column(
-//                     children: [
-//                       if (state is GetProductLoadingState)
-//                         const CircularProgressIndicator(color: Colors.white),
-//                       if (state is! GetProductLoadingState)
-//                         InkWell(
-//                           onTap: () {
-//                             Navigator.of(context).push(
-//                               MaterialPageRoute(
-//                                 builder: (context) => OrdersScreen(
-//                                   orders: BlocProvider.of<ProductCubit>(context)
-//                                       .orders,
-//                                 ),
-//                               ),
-//                             );
-//                           },
-//                           child: Container(
-//                             margin: const EdgeInsets.all(8),
-//                             decoration: BoxDecoration(
-//                               color: AppStyles.kPrimaryColor,
-//                               borderRadius: BorderRadius.circular(12.0),
-//                             ),
-//                             child: const ListTile(
-//                               title: Text(
-//                                 'Orders ',
-//                                 style: TextStyle(color: Colors.white , fontSize: 18),
-//                               ),
-//                               trailing: Icon(
-//                                 // Icons.receipt_long ,
-//                                 Icons.assignment ,
-//                                 color: Colors.white,
-//                               ),
-//                             ),
-//                           ),
-//                         ),
-//                       if (state is DeleteProductLoadingState)
-//                         const Padding(
-//                           padding: EdgeInsets.all(20),
-//                           child: LinearProgressIndicator(),
-//                         ),
-//                       Expanded(
-//                         child: RefreshIndicator(
-//                           onRefresh: () async {
-//                             cubit.fetchAllProducts();
-//                           },
-//                           child: ListView.separated(
-//                             physics: const BouncingScrollPhysics(),
-//                             itemBuilder: (context, index) {
-//                               return ProductListViewItem(
-//                                 product: cubit.products[index],
-//                               );
-//                             },
-//                             separatorBuilder: (context, index) =>
-//                                 const SizedBox(height: 8),
-//                             itemCount: cubit.products.length,
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   );
-//                 }
-//               }
-//             },
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Footwear Products"),
-//         centerTitle: true,
-//         actions: [
-//           IconButton(
-//             onPressed: () {
-//               Navigator.of(context).push(
-//                 MaterialPageRoute(
-//                   builder: (context) => const AddProductScreen(),
-//                 ),
-//               );
-//             },
-//             icon: const Icon(Icons.add),
-//           ),
-//         ],
-//       ),
-//       body: Column(
-//         crossAxisAlignment: CrossAxisAlignment.stretch,
-//         children: [
-//           Container(
-//             padding: const EdgeInsets.all(20),
-//             decoration: BoxDecoration(
-//               color: Colors.blueGrey[50],
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//             margin: const EdgeInsets.all(10),
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 InkWell(
-//                   onTap: () {
-//                     Navigator.of(context).push(
-//                       MaterialPageRoute(
-//                         builder: (context) => OrdersScreen(
-//                           orders: BlocProvider.of<ProductCubit>(context).orders,
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                   child: Column(
-//                     children: [
-//                       Icon(Icons.assignment, size: 50, color: Colors.blue),
-//                       const SizedBox(height: 5),
-//                       Text(
-//                         'Orders',
-//                         style: TextStyle(fontSize: 16, color: Colors.blue),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 InkWell(
-//                   onTap: () {
-//                     Navigator.of(context).push(
-//                       MaterialPageRoute(
-//                         builder: (context) => const AddProductScreen(),
-//                       ),
-//                     );
-//                   },
-//                   child: Column(
-//                     children: [
-//                       Icon(Icons.add_shopping_cart, size: 50, color: Colors.green),
-//                       const SizedBox(height: 5),
-//                       Text(
-//                         'Add Product',
-//                         style: TextStyle(fontSize: 16, color: Colors.green),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Expanded(
-//             child: BlocBuilder<ProductCubit, ProductStates>(
-//               builder: (context, state) {
-//                 var cubit = BlocProvider.of<ProductCubit>(context);
-//                 List<ProductModel> products = cubit.products;
-//                 return GridView.builder(
-//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                     crossAxisCount: 2,
-//                     crossAxisSpacing: 10,
-//                     mainAxisSpacing: 10,
-//                     childAspectRatio: 0.7,
-//                   ),
-//                   itemCount: products.length,
-//                   itemBuilder: (context, index) {
-//                     return ProductCard(product: products[index]);
-//                   },
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-//
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -260,7 +22,17 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         scrolledUnderElevation: 0,
       ),
-      body: BlocBuilder<ProductCubit, ProductStates>(
+      body: BlocConsumer<ProductCubit, ProductStates>(
+        listener: (context, state) {
+          if (state is DeleteProductSuccessState) {
+            CustomSnackBarOverlay.show(
+              context,
+              message: 'Deleted ',
+              messageDescription: 'Product Deleted Successfully',
+              msgColor: Colors.red
+            );
+          }
+        },
         builder: (context, state) {
           var cubit = BlocProvider.of<ProductCubit>(context);
           List<ProductModel> products = cubit.products;
@@ -490,11 +262,11 @@ class ProductGridViewItem extends StatelessWidget {
                     },
                     child: const CircleAvatar(
                       backgroundColor: Colors.red,
-                      radius: 20,
+                      radius: 16,
                       child: Icon(
                         Icons.delete,
                         color: Colors.white,
-                        size: 20,
+                        size: 16,
                       ),
                     ),
                   ),
