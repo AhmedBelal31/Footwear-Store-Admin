e29c8d636cf9d0478b4f3ba43ecf99f85f6ef4c5
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-09 11:25:56 +0300

    Products Grid View Banner

--- lib/presentation/widgets/home_screen_widgets/products_grid_view_banner.dart
+++ lib/presentation/widgets/home_screen_widgets/products_grid_view_banner.dart
@@ -15,13 +15,14 @@ class ProductsGridViewBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     return SizedBox(
-        height: screenHeight * 0.180,
-        width: double.infinity,
-        child: FancyShimmerImage(
-          imageUrl: product.imageUrl ??
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png',
-          errorWidget: Image.network(
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'),
-        ));
+      height: screenHeight * 0.180,
+      width: double.infinity,
+      child: FancyShimmerImage(
+        imageUrl: product.imageUrl ??
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png',
+        errorWidget: Image.network(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'),
+      ),
+    );
   }
 }
