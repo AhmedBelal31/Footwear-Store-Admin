ba085f69eb46aa9c354defb388548b6256566286
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-06-12 15:00:42 +0300

    Calling Delete Order

--- lib/presentation/widgets/orders_screen_widgets/alert_dialog_delete_order.dart
+++ lib/presentation/widgets/orders_screen_widgets/alert_dialog_delete_order.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:footwear_store_admin/presentation/controller/product_cubit.dart';
 import '../../../data/models/order_product_model.dart';
 
 class AlertDialogDeleteOrder extends StatefulWidget {
@@ -17,6 +19,7 @@ class AlertDialogDeleteOrderState extends State<AlertDialogDeleteOrder> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+
       title: const Text('Delete Order'),
       content: const Text('Are you sure you want to delete this order?'),
       actions: [
@@ -28,7 +31,7 @@ class AlertDialogDeleteOrderState extends State<AlertDialogDeleteOrder> {
         ),
         TextButton(
           onPressed: () {
-
+            BlocProvider.of<ProductCubit>(context).deleteOrder(orderId: widget.order.orderId);
             ///Delete Order ..
             Navigator.of(context).pop(true); // Return true to confirm dismissal
           },
@@ -38,3 +41,7 @@ class AlertDialogDeleteOrderState extends State<AlertDialogDeleteOrder> {
     );
   }
 }
+
+
+
+
